b2b4a7132fe04d8d1dd2dcfefb78949736fc28b6
Brie Bencich <bbencich@hotmail.com>
2021-02-18 14:06:03 +0200

    adding connection

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -29,6 +29,41 @@ const indexedDB =
     }; 
     
     function saveRecord(record) { 
-        const transaction = db.tranaction(["pending"], )
+        const transaction = db.tranaction(["pending"], "readwrite"); 
+        const store = transaction.objectStore("pending");
+
+        store.add(record); 
     }
+
+
+    function checkDatabase() { 
+        const transaction = db.transaction(["pending"], "readwrite"); 
+        const store = transaction.objectStore("pending"); 
+        const getAll = store.getAll();
+
+        getAll.onsuccess = function() { 
+            if (getAll.result.length > 0 ) { 
+                fetch("/api/transaction/bulk", { 
+                    method: "POST",
+                    body: JSON.stringify(getAll.result), 
+                    headers: { 
+                        Accept: "application/json, text/plain, */*", 
+                    "Content-Type": "application/json"
+                    }
+                })
+                .then(response => { 
+                    return response.json(); 
+                })
+                .then(() => { 
+                    const transaction = db.transaction(["pending"], "readwrite"); 
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+            }
+        };
+
+    }
+
+
+window.addEventListener("online", checkDatabase); 
 
